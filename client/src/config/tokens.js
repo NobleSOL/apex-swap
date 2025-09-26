1b85fb9923150d2e900fef939adf70d0f45713ab
Noble SOL <155856038+NobleSOL@users.noreply.github.com>
2025-09-26 13:20:51 -0700

    Set KTA as default swap token with branding

---
+++ client/src/config/tokens.js
@@ -0,0 +1,11 @@
+import ktaLogo from "../assets/kta.svg";
+
+export const TOKENS = {
+  KTA: {
+    symbol: "KTA",
+    name: "Keeta Native Token",
+    logo: ktaLogo,
+    isNative: true,
+  },
+  // Additional tokens can be added here later
+};
