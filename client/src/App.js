77384c530311ab2e98a10510deaf0d5a4652522c
Noble SOL <155856038+NobleSOL@users.noreply.github.com>
2025-09-25 22:18:38 -0700

    Fix syntax error in wallet connect handler

--- client/src/App.js
+++ client/src/App.js
@@ -406,13 +406,7 @@ function WalletControls({ wallet, onWalletChange }) {
       if (!/^[0-9a-fA-F]{64}$/.test(trimmed)) {
         throw new Error("Provide a 64-character hexadecimal seed");
       }
-codex/fix-website-issue-with-seed-paste-hu2rah
 
-codex/fix-website-issue-with-seed-paste-v98bbq
-
-      const index = Number(indexInput) || 0;
-master
-master
       const account = KeetaLib.Account.fromSeed(trimmed, index);
       const address = account.publicKeyString.get();
 
